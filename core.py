a8b2163f87e149df8e80ba952f6ba51a49cea25f
Siennaaaa <sekhn99@163.com>
2020-03-11 20:25:21 +0800

    fixed some bugs

--- core.py
+++ core.py
@@ -14,6 +14,7 @@ import env as Env
 from tensorboardX import SummaryWriter
 
 from lib import agent, action, experience, parameters, saver, tracker, wrappers
+import env as Env
 
 # Global Variable:
 params = parameters.Constants
@@ -80,17 +81,17 @@ class DeepNetwork:
             # eval_net 的第一层. collections 是在更新 target_net 参数时会用到,n_l1=256是我决定的
             with tf.variable_scope('l1-conv1'):
                 # 第一层为卷积层，过滤器大小为5x5，深度为6，不够用0补全。步长为1。
-                conv1_weights = tf.get_variable('weight', [5, 5, c, 6],
-                                                initializer=tf.truncated_normal_initializer(stddev=0.1))
-                conv1_biases = tf.get_variable('bias', [6], initializer=tf.constant_initializer(0.0))
-                conv1 = tf.nn.conv2d(inputs, conv1_weights, strides=[1, 1, 1, 1], padding='VALID')
+                conv1_weights = tf.get_variable('weight', [5, 5, 1, 6],
+                                                initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
+                conv1_biases = tf.get_variable('bias', [6], initializer=tf.constant_initializer(0.0), collections=c_names)
+                conv1 = tf.nn.conv2d(inputs, conv1_weights, strides=[1, 2, 2, 1], padding='VALID')
                 relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
 
                 # w1 = tf.get_variable('w1', [self.n_features, 256], initializer=w_initializer, collections=c_names)
                 # b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
                 # l1 = tf.nn.relu(tf.matmul(self.s, w1) + b1)
 
-            # eval_net 的第二层. collections 是在更新 target_net 参数时会用到.第二层为池化层，步长为1，不够用0补全。
+            # eval_net 的第二层. collections 是在更新 target_net 参数时会用到.第二层为池化层，步长为2，不够用0补全。这里的input是116x104
             with tf.variable_scope('l2-pool1'):
                 pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
                 # conv1 = tf.layers.con2d(l1,filters=16,kernel_size=5,strides=1,padding='same',activation=tf.nn.relu)
@@ -100,9 +101,9 @@ class DeepNetwork:
 
             with tf.variable_scope('l3-conv2'):
                 conv2_weights = tf.get_variable('weight', [5, 5, 6, 16],
-                                                initializer=tf.truncated_normal_initializer(stddev=0.1))
-                conv2_biases = tf.get_variable('bias', [16], initializer=tf.constant_initializer(0.0))
-                conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 1, 1, 1], padding='VALID')
+                                                initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
+                conv2_biases = tf.get_variable('bias', [16], initializer=tf.constant_initializer(0.0), collections=c_names)
+                conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 2, 2, 1], padding='VALID')
                 relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
                 # l1 = tf.layers.dense(inputs,units=100,activation=tf.nn.relu)
 
@@ -125,10 +126,10 @@ class DeepNetwork:
             # 最后训练时没有采用dropout，dropout项传入参数设置成了False，因为训练和测试写在了一起没有分离，不过可以尝试。
             with tf.variable_scope('l5-fc1'):
                 fc1_weights = tf.get_variable('weight', [nodes, 512],
-                                              initializer=tf.truncated_normal_initializer(stddev=0.1))
+                                              initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
                 # if regularizer != None:
                 #     tf.add_to_collection('losses', regularizer(fc1_weights))
-                fc1_biases = tf.get_variable('bias', [512], initializer=tf.constant_initializer(0.1))
+                fc1_biases = tf.get_variable('bias', [512], initializer=tf.constant_initializer(0.1), collections=c_names)
                 fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
 
                 # if train:
@@ -176,34 +177,133 @@ class DeepNetwork:
             # c_names(collections_names) 是在更新 target_net 参数时会用到
             c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
 
-            # target_net 的第一层. collections 是在更新 target_net 参数时会用到
-            with tf.variable_scope('l1'):
-                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
-                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
-                l1 = tf.nn.relu(tf.matmul(self.s_, w1) + b1)
+            # target_net 的第一层. collections 是在更新 target_net 参数时会用到,n_l1=256是我决定的
+            with tf.variable_scope('l1-conv1'):
+                # 第一层为卷积层，过滤器大小为5x5，深度为6，不够用0补全。步长为1。
+                conv1_weights = tf.get_variable('weight', [5, 5, c, 6],
+                                                initializer=tf.truncated_normal_initializer(stddev=0.1))
+                conv1_biases = tf.get_variable('bias', [6], initializer=tf.constant_initializer(0.0))
+                conv1 = tf.nn.conv2d(inputs, conv1_weights, strides=[1, 2, 2, 1], padding='VALID')
+                relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
 
-            # target_net 的第二层. collections 是在更新 target_net 参数时会用到
-            with tf.variable_scope('l2'):
-                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
-                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
-                self.q_next = tf.matmul(l1, w2) + b2
+                # w1 = tf.get_variable('w1', [self.n_features, 256], initializer=w_initializer, collections=c_names)
+                # b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+                # l1 = tf.nn.relu(tf.matmul(self.s, w1) + b1)
+
+            # target_net 的第二层. .第二层为池化层，步长为2，不够用0补全。
+            with tf.variable_scope('l2-pool1'):
+                pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+                # conv1 = tf.layers.con2d(l1,filters=16,kernel_size=5,strides=1,padding='same',activation=tf.nn.relu)
+                # w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                # b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                # self.q_eval = tf.matmul(l1, w2) + b2
+
+            with tf.variable_scope('l3-conv2'):
+                conv2_weights = tf.get_variable('weight', [5, 5, 6, 16],
+                                                initializer=tf.truncated_normal_initializer(stddev=0.1))
+                conv2_biases = tf.get_variable('bias', [16], initializer=tf.constant_initializer(0.0))
+                conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 2, 2, 1], padding='VALID')
+                relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+                # l1 = tf.layers.dense(inputs,units=100,activation=tf.nn.relu)
+
+            with tf.variable_scope('l4-pool2'):
+                pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 1, 1, 1], padding='SAME')
+                # l1 = tf.layers.dense(inputs,units=100,activation=tf.nn.relu)
+
+            # 将第四层池化层的输出转化为第五层全连接层的输入格式。第四层的输出为5×5×16的矩阵，然而第五层全连接层需要的输入格式
+            # 为向量，所以我们需要把代表每张图片的尺寸为5×5×16的矩阵拉直成一个长度为5×5×16的向量。
+            # 举例说，每次训练64张图片，那么第四层池化层的输出的size为(64,5,5,16),拉直为向量，nodes=5×5×16=400,尺寸size变为(64,400)
+
+            pool_shape = pool2.get_shape().as_list()
+            nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
+            reshaped = tf.reshape(pool2, [-1, nodes])
+
+            # 第五层：全连接层，nodes=5×5×16=400，400->120的全连接
+            # 训练时，引入dropout，dropout在训练时会随机将部分节点的输出改为0，dropout可以避免过拟合问题。
+            # 这和模型越简单越不容易过拟合思想一致，和正则化限制权重的大小，使得模型不能任意拟合训练数据中的随机噪声，以此达到避免过拟合思想一致。
+            # 最后训练时没有采用dropout，dropout项传入参数设置成了False，因为训练和测试写在了一起没有分离，不过可以尝试。
+            with tf.variable_scope('l5-fc1'):
+                fc1_weights = tf.get_variable('weight', [nodes, 512],
+                                              initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
+                # if regularizer != None:
+                #     tf.add_to_collection('losses', regularizer(fc1_weights))
+                fc1_biases = tf.get_variable('bias', [512], initializer=tf.constant_initializer(0.1), collections=c_names)
+                fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
+
+                # if train:
+                #     fc1 = tf.nn.dropout(fc1, 0.5)
+
+            # 第六层：全连接层，120->84的全连接
+            # 尺寸变化：比如一组训练样本为64，那么尺寸变化为64×120->64×84
+            with tf.variable_scope('l6-fc2'):
+                fc2_weights = tf.get_variable('weight', [512, 256],
+                                              initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
+                # if regularizer != None:
+                #     tf.add_to_collection('losses', regularizer(fc2_weights))
+                fc2_biases = tf.get_variable('bias', [256], initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
+                fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
+
+                # if train:
+                #     fc2 = tf.nn.dropout(fc2, 0.5)
+
+            # 第七层：全连接层（近似表示），84->10的全连接
+            # 尺寸变化：比如一组训练样本为64，那么尺寸变化为64×84->64×10。最后，64×10的矩阵经过softmax之后就得出了64张图片分类于每种数字的概率，
+            # 即得到最后的分类结果。
+            with tf.variable_scope('l7-fc3'):
+                fc3_weights = tf.get_variable('weight', [256, 8],
+                                              initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
+                # if regularizer != None:
+                #     tf.add_to_collection('losses', regularizer(fc3_weights))
+                fc3_biases = tf.get_variable('bias', [8], initializer=tf.truncated_normal_initializer(stddev=0.1), collections=c_names)
+                self.q_next = tf.nn.relu(fc2, fc3_weights) + fc3_biases
+            return self.q_next
+
+
+            # # target_net 的第一层. collections 是在更新 target_net 参数时会用到
+            # with tf.variable_scope('l1'):
+            #     w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+            #     b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+            #     l1 = tf.nn.relu(tf.matmul(self.s_, w1) + b1)
+            #
+            # # target_net 的第二层. collections 是在更新 target_net 参数时会用到
+            # with tf.variable_scope('l2'):
+            #     w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+            #     b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+            #     self.q_next = tf.matmul(l1, w2) + b2
 
 
     def store_transition(self):
 
         pass
 
-    def choose_action(self):
-        batch_size, n_actions = self.q_eval.shape
-        actions = self.selector(self.q_eval)
-        mask = np.random.random(size=batch_size) < self.epsilon
-        rand_actions = np.random.choice(n_actions, sum(mask))
-        actions[mask] = rand_actions
-        return actions
+# def choose_action(self):
+#     batch_size, n_actions = self.q_eval.shape
+#     actions = self.selector(self.q_eval)
+#     mask = np.random.random(size=batch_size) < self.epsilon
+#     rand_actions = np.random.choice(n_actions, sum(mask))
+#     actions[mask] = rand_actions
+#     return actions
 
-    def learn(self):
+def learn(frameskip,device='CUDA'):
+    env = Env.SumoEnv(frameskip=frameskip,device=device)
+    # env = wrappers.wrap_dqn(env, reward_clipping=False)
+    state_size = env.observation_space
+    action_size = env.action_space
+    net = DeepNetwork(n_actions=action_size,n_features=state_size)
+    # Training initialization
+    selector = action.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
+    #take care of params! I have defined some params in class instructor but some had been in lib!
+    epsilon_tracker = tracker.EpsilonTracker(selector, params)
+    tgt_net = agent.TargetNet(net)
+    agents = agent.DQNAgent(net, selector, device=device)
+    #experice retrieve
+    exp_source = experience.ExperienceSourceFirstLast(env, agents, gamma=params['gamma'], steps_count=1)
 
-        pass
+
+
+
+
+    pass
 
 # if __name__ == '__main__':
 #     parser = argparse.ArgumentParser()
