12efc3923b72f9a09f9b445ea35c8f40e56467f7
ChocolateDave <45930715+ChocolateDave@users.noreply.github.com>
2020-04-06 15:30:12 +0800

    Fixed bug
    #1 Place seeding.py in a correct position

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from * import agent
 from * import env
 from * import memory
+from * import seeding
 from * import settings
 
-__all__ = ["agent","env", "memory", "settings"]
+__all__ = ["agent","env", "memory", "seeding", "settings"]
--- env.py
+++ env.py
@@ -7,7 +7,7 @@ if 'SUMO_HOME' in os.environ:
     sys.path.append(tools)
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
-from lib import seeding
+import seeding
 from sumolib import checkBinary
 import gym
 import numpy as np
